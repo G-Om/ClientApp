36921cb731115e85b1c6e7a4f282b6333cf015be
om.gavade <115257951+G-Om@users.noreply.github.com>
2023-07-10 23:37:04 +0530

    Done naming

---
+++ demo/src/main/java/com/client/ClientApplication.java
@@ -0,0 +1,13 @@
+package com.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+// ehk;sd
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
+}
